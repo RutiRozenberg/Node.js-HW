d0a67677cfbf7abb730b9e6984afb19d150fa09e
RutiRozenberg <ruti7633@gmail.com>
2024-05-23 15:15:11 +0300

    Connecting and creating the database.

---
+++ services/categoryService.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+
+const Schema = mongoose.Schema;
+
+const categorySchema = new Schema({
+    id: Number,
+    category: String,
+    products: [{
+        id: Number,
+        name: String,
+        price: Number
+    }]
+});
+
+const ProductModel = mongoose.model("products", categorySchema);
+
+module.exports = { ProductModel};
---
+++ services/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+mongoose.set("strictQuery", false);
+
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/sale')
+}
+
+
+
+module.exports = main
